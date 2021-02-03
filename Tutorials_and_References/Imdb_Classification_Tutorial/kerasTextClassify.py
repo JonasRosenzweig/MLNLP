a4ccbe946e8c0c2483e8bec96f97136f4b17e0c0
JonasRosenzweig <Jdrosenweig@gmail.com>
2021-02-03 23:37:25 +0100

    Tutorials/Reference Code initial commit - New Branch

---
+++ Tutorials_and_References/Imdb_Classification_Tutorial/kerasTextClassify.py
@@ -0,0 +1,157 @@
+# from https://keras.io/examples/nlp/text_classification_from_scratch/
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignore TensorFlow warnings
+import tensorflow as tf
+import numpy as np
+import string
+import re
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+
+
+
+# You can use the utility tf.keras.preprocessing.text_dataset_from_directory to generate
+# a labeled tf.data.Dataset object from a set of text files on disk filed into class-specific folders.
+# Let's use it to generate the training, validation, and test datasets. The validation and training
+# datasets are generated from two subsets of the train directory, with 20% of samples going to the
+# validation dataset and 80% going to the training dataset. Before putting the model out into the
+# real world however,  it should be retrained using all available training data
+# (without creating a validation dataset), so its performance is maximized.
+batch_size = 32
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/train",
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+)
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/train",
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+)
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/test", batch_size=batch_size
+)
+
+print(
+    "Number of batches in raw_train_ds: %d"
+    % tf.data.experimental.cardinality(raw_train_ds)
+)
+print(
+    "Number of batches in raw_val_ds: %d" % tf.data.experimental.cardinality(raw_val_ds)
+)
+print(
+    "Number of batches in raw_test_ds: %d"
+    % tf.data.experimental.cardinality(raw_test_ds)
+)
+
+# It's important to take a look at your raw data to ensure your normalization and tokenization will work as expected.
+# We can do that by taking a few examples from the training set and looking at them.
+# This is one of the places where eager execution shines:
+# we can just evaluate these tensors using .numpy()  instead of needing to evaluate them in a Session/Graph context.
+# .numpy() converts a tensor object into a numpy ndarray object
+for text_batch, label_batch in raw_train_ds.take(1):
+    for i in range(5):
+        print(text_batch.numpy()[i])
+        print(label_batch.numpy()[i])
+
+
+# Having looked at our data above, we see that the raw text contains HTML break
+# tags of the form '<br />'. These tags will not be removed by the default
+# standardizer (which doesn't strip HTML). Because of this, we will need to
+# create a custom standardization function.
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
+    return tf.strings.regex_replace(stripped_html, "[%s]" % re.escape(string.punctuation), "")
+
+
+# Model constants.
+max_features = 20000
+embedding_dim = 128
+sequence_length = 500
+
+# Now that we have our custom standardization, we can instantiate our text
+# vectorization layer. We are using this layer to normalize, split, and map
+# strings to integers, so we set our 'output_mode' to 'int'.
+# Note that we're using the default split function,
+# and the custom standardization defined above.
+# We also set an explicit maximum sequence length, since the CNNs later in our
+# model won't support ragged sequences.
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode="int",
+    output_sequence_length=sequence_length,
+)
+
+# Now that the vocab layer has been created, call `adapt` on a text-only
+# dataset to create the vocabulary. You don't have to batch, but for very large
+# datasets this means you're not keeping spare copies of the dataset in memory.
+
+# Let's make a text-only dataset (no labels):
+text_ds = raw_train_ds.map(lambda x, y: x)
+# Let's call `adapt`:
+vectorize_layer.adapt(text_ds)
+
+
+# Vectorizing data option 2 (apply it to the text dataset)
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+# Vectorize the data.
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+# Do async prefetching / buffering of the data for best performance on GPU.
+train_ds = train_ds.cache().prefetch(buffer_size=10)
+val_ds = val_ds.cache().prefetch(buffer_size=10)
+test_ds = test_ds.cache().prefetch(buffer_size=10)
+
+# Building the Model (this is the Neural Network)
+# we choose a simple 1D convnet starting with an embedding layer
+
+# A integer input for vocab indices.
+inputs = tf.keras.Input(shape=(None,), dtype="int64")
+
+# Next, we add a layer to map those vocab indices into a space of dimensionality
+# 'embedding_dim'.
+x = layers.Embedding(max_features, embedding_dim)(inputs)
+x = layers.Dropout(0.5)(x)
+
+# Conv1D + global max pooling
+x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
+x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
+x = layers.GlobalMaxPooling1D()(x)
+
+# We add a vanilla hidden layer:
+x = layers.Dense(128, activation="relu")(x)
+x = layers.Dropout(0.5)(x)
+
+# We project onto a single unit output layer, and squash it with a sigmoid:
+predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
+
+model = tf.keras.Model(inputs, predictions)
+
+# Compile the model with binary crossentropy loss and an adam optimizer.
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# show summary of model shape
+model.summary()
+
+# Training the model
+epochs = 1
+
+# Fit the model using the train and test datasets.
+model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+
+# Evaluating the model
+model.evaluate(test_ds)
+
+
