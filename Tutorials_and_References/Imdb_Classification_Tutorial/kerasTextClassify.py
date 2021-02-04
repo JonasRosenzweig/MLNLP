d3a39bcc44d42ff1bc8d1e25ac2321f952f96315
Daniel <Danielnoren2@gmail.com>
2021-02-04 15:42:15 +0100

    Changed path in kerasTextClassify.py

--- Tutorials_and_References/Imdb_Classification_Tutorial/kerasTextClassify.py
+++ Tutorials_and_References/Imdb_Classification_Tutorial/kerasTextClassify.py
@@ -20,21 +20,21 @@ from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 # (without creating a validation dataset), so its performance is maximized.
 batch_size = 32
 raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/train",
+    "aclImdb/train",
     batch_size=batch_size,
     validation_split=0.2,
     subset="training",
     seed=1337,
 )
 raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/train",
+    "aclImdb/train",
     batch_size=batch_size,
     validation_split=0.2,
     subset="validation",
     seed=1337,
 )
 raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    "Tutorials_and_References/Imdb_Classification_Tutorial/aclImdb/test", batch_size=batch_size
+    "aclImdb/test", batch_size=batch_size
 )
 
 print(
