187001dd858cf4a0632aa43772970713bb65c00c
JonasRosenzweig <Jdrosenweig@gmail.com>
2021-02-08 16:16:31 +0100

    added Chapter 1 code example TF

---
+++ Tutorials_and_References/Applied_NLP_book_code/Chapter1_TF.py
@@ -0,0 +1,4 @@
+# Applied Natural Language Processing with Python: Implementing Machine Learning
+# and Deep Learning Algorithms for Natural Language Processing, by Taweh Beysolow II
+# Chapter 1 - TF code examples
+
