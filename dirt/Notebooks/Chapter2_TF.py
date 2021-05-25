c36f801a7fe70b1ed02f1907937003fc33edcaaf
Hammi007 <h.e.4000@hotmail.com>
2021-05-25 11:10:30 +0200

    Final touches - removed redundant directory

--- dirt/Notebooks/Chapter2_TF.py
+++
@@ -1,8 +0,0 @@
-# Applied Natural Language Processing with Python: Implementing Machine Learning
-# and Deep Learning Algorithms for Natural Language Processing, by Taweh Beysolow II
-# Chapter 2 - TF code example - Ford motor company
-
-import tensorflow as tf
-train_data = []
-
-# def mlp_model(train_data=train_data, learning_rate = 0.01, iters=100, num_hidden1=256):
